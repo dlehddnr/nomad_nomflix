8d05e4842a190c8d28030182ea86b4012e52f6eb
mac <mac@macui-MacBook-Pro.local>
2019-05-07 18:44:12 +0900

    #3.9

--- src/Componenets/Header.js
+++ src/Componenets/Header.js
@@ -1,32 +1,53 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
-const Header = styled.header``;
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0px 10px;
+  background-color: rgba(20, 20, 20, 0.8);
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
 
 const List = styled.ul`
   display: flex;
-  &:hover {
-    background-color: blue;
-  }
 `;
 
-const Item = styled.li``;
+const Item = styled.li`
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  border-bottom: 5px solid
+    ${props => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom 0.5s ease-in-out;
+`;
 
-const Slink = styled(Link)``;
+const Slink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-export default () => (
-  <header>
+export default withRouter(({ location: { pathname } }) => (
+  <Header>
     <List>
-      <li>
-        <Slink href="/">Movies </Slink>
-      </li>
-      <li>
-        <Slink href="/tv">TV </Slink>
-      </li>
-      <li>
-        <Slink href="/search">Search </Slink>
-      </li>
+      <Item current={pathname === "/"}>
+        <Slink to="/">Movies </Slink>
+      </Item>
+      <Item current={pathname === "/tv"}>
+        <Slink to="/tv">TV </Slink>
+      </Item>
+      <Item current={pathname === "/search"}>
+        <Slink to="/search">Search </Slink>
+      </Item>
     </List>
-  </header>
-);
+  </Header>
+));
