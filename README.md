9a616d5decd0377cd0269d233234a6ba00ffcd70
mac <dy95032@gmail.com>
2019-05-09 07:13:05 +0900

    chapter 4

--- README.md
+++ README.md
@@ -7,4 +7,12 @@
 - [ ] Search
 - [ ] Detail
 
-react router dom
+## API Verbs
+
+- [x] Now Playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV)
+- [x] Popular (TV, Movie)
+- [x] Airing Today (TV)
+- [x] Detail
+- [x] Search
---
+++ src/api.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "0e4a505a70b486f6faff49ba2fd6c61d",
+    language: "en-US"
+  }
+});
+
+export const moviesApi = {
+    nowPlaying = () => api.get("movie/now_playing"),
+    upComing = () => api.get("movie/upcoming"),
+    popular = () => api.get("movie/popular"),
+    movieDetail = (id) => api.get(`movie/${id}`, {
+        params: {
+            append_to_response: "videos"
+        }
+    }),
+    search = (term) => api.get("search/movie",
+    {
+        params : {
+            query: encodedURIComponent(term)
+        }
+    })
+}
+
+export const tvApi = {
+    topRated = () => api.get("tv/top_rated"),
+    popular = () => api.get("tv/popular"),
+    airingToday = () => api.get("tv/airing_today"),
+    tvDetail = (id) => api.get(`tv/${id}`, {
+        params: {
+            append_to_response: "videos"
+        }
+    }),
+    search = (term) => api.get("search/tv",
+    {
+        params : {
+            query: encodedURIComponent(term)
+        }
+    })
+}
+
